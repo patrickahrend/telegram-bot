b0b97cbbfb6748846671c60723f61e1647f6ac8d
Patrick Ahrend <patrickahrend@yahoo.com>
2022-03-08 09:26:06 +0100

    feat:add google auth

--- src/Responses.py
+++ src/Responses.py
@@ -1,11 +1,87 @@
-def sample_response(input_text):
-    user_message = str(input_text).lower()
+import re
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+import os
+import constants as c
 
-    if user_message in ("hello", "hi", "sup"):
-        return "Hey! How is it going?"
 
-    if user_message in ("who are you", "who are you?"):
-        return "I am a telegram bot"
+GOOGLE_AUTH = c.GOOGLE_AUTH
+SCOPES = ["https://www.googleapis.com/auth/contacts.readonly"]
 
-    return "I did not get that"
+def process_message(message, response_array, response):
+    # Splits the message and the punctuation into an array
+    list_message = re.findall(r"[\w']+|[.,!?;]", message.lower())
 
+    # Scores the amount of words in the message
+    score = 0
+    for word in list_message:
+        if word in response_array:
+            score = score + 1
+
+    # Returns the response and the score of the response
+    # print(score, response)
+    return [score, response]
+
+
+def get_response(message):
+    # Add your custom responses here
+    response_list = [
+        process_message(message, ["hello", "hi", "hey"], "Hey there!"),
+        process_message(message, ["bye", "goodbye"], "Goodbye!"),
+        process_message(message, ["how", "are", "you"], "I'm doing fine thanks!"),
+        
+        process_message(
+            message, ["your", "name"], "My name is Farah, nice to meet you!"
+        ),
+        process_message(message, ["help", "me"], "I will do my best to assist you!"),
+        process_message(message, ["fahard"], google_connection())
+        # Add more responses here
+    ]
+
+    # Checks all of the response scores and returns the best matching response
+    response_scores = []
+    for response in response_list:
+        response_scores.append(response[0])
+
+    # Get the max value for the best response and store it into a variable
+    winning_response = max(response_scores)
+    matching_response = response_list[response_scores.index(winning_response)]
+
+    # Return the matching response to the user
+    if winning_response == 0:
+        bot_response = "I didn't understand what you wrote."
+    else:
+        bot_response = matching_response[1]
+
+    print("Bot response:", bot_response)
+    return bot_response
+
+
+def google_connection():
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    creds = c.GOOGLE_AUTH
+
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return creds
+
+
+# Test your system
+# get_response('What is your name bruv?')
+# get_response('Can you help me with something please?')
--- src/main.py
+++ src/main.py
@@ -1,43 +1,63 @@
-# pylint: disable=missing-module-docstring
-import Constants as keys
+import logging
 from telegram.ext import *
-import Responses as R
+import responses
+import constants as c
 
-print("Bot started....")
+
+API_KEY = c.API_KEY
+
+
+# Set up the logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.info("Starting Bot...")
 
 
 def start_command(update, context):
-    update.message.reply_text("Type something random to get started!")
+    update.message.reply_text("Hello there! I'm a bot. What's up?")
 
 
 def help_command(update, context):
-    update.message.reply_text("If you need help... ")
+    update.message.reply_text("Try typing anything and I will do my best to respond!")
+
+
+def custom_command(update, context):
+    update.message.reply_text(
+        "This is a custom command, you can add whatever text you want here."
+    )
 
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
-    response = R.sample_response(text)
+    logging.info(f"User ({update.message.chat.id}) says: {text}")
 
-    update.messag.reply_text(response)
-
-
-def error(update):
-    print(f"Update{update} caused error {context.error}")
+    # Bot response
+    response = responses.get_response(text)
+    update.message.reply_text(response)
 
 
-def main():
-    updater = Updater(keys.API_KEY, use_context=True)
+def error(update, context):
+    # Logs errors
+    logging.error(f"Update {update} caused error {context.error}")
+
+
+# Run the programme
+if __name__ == "__main__":
+    updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
 
+    # Commands
     dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("start", help_command))
+    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("custom", custom_command))
 
+    # Messages
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
+    # Log all errors
     dp.add_error_handler(error)
-    updater.start_polling()
+
+    # Run the bot
+    updater.start_polling(1.0)
     updater.idle()
-
-
-main()
-
